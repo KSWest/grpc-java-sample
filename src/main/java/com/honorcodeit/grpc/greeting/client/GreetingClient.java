1c965fd5e470ec3b9f732c98e738a73f5bad5aa6
Mykola Shumyn <shumyn.kolya@gmail.com>
2018-09-29 15:08:01 +0300

    - implemented gRPC Unary

--- src/main/java/com/honorcodeit/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/honorcodeit/grpc/greeting/client/GreetingClient.java
@@ -1,6 +1,9 @@
 package com.honorcodeit.grpc.greeting.client;
 
-import com.proto.dummy.DummyServiceGrpc;
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.Greeting;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -13,9 +16,29 @@ public class GreetingClient {
                 .build();
 
         System.out.println("Creating stub");
-        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
 
+        // old and dummy
+        // DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
         // DummyServiceGrpc.DummyServiceFutureStub asyncClient = DummyServiceGrpc.newFutureStub(channel);
+
+        // create a greet service client (blocking - synchronous)
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
+
+        // create a protocol buffer greeting message
+        Greeting greeting = Greeting.newBuilder()
+                .setFirstName("John")
+                .setLastName("Dou")
+                .build();
+
+        // do the same for a GreetRequest
+        GreetRequest greetRequest = GreetRequest.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        // call the RPC and get back a GreetResponse (protocol buffers)
+        GreetResponse response = greetClient.greet(greetRequest);
+
+        System.out.println(response.getResult());
 
         // do smth
         System.out.println("Shutting down channel");
